5586c015f0e27152af1b9f48a203a8c4a799b01d
Saša Mitrović <sasha.mitrovich@hotmail.com>
2020-10-02 15:51:28 +0200

    Initial Commit

---
+++ ShareData/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShareData
+//
+//  Created by Saša Mitrović on 02.10.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShareData/ShareDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareDataApp.swift
+//  ShareData
+//
+//  Created by Saša Mitrović on 02.10.20.
+//
+
+import SwiftUI
+
+@main
+struct ShareDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
